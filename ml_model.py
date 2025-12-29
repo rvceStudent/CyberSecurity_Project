2d6fb230b2b1eade268f400cc989018d7926ce18
rvceStudent <leelanjalip2003@gmail.com>
2025-12-29 20:14:06 +0530

    Add files via upload

---
+++ ml_model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class AttackDetector(nn.Module):
+    def __init__(self):
+        super(AttackDetector, self).__init__()
+        self.fc1 = nn.Linear(5, 16)
+        self.fc2 = nn.Linear(16, 8)
+        self.fc3 = nn.Linear(8, 1)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.sigmoid(self.fc3(x))
+        return x
